e07c4c2f50fcb8b9b0e9aea71a06ba5c0cc23a99
ijse <i@ijser.cn>
2018-02-13 23:13:02 +0800

    add test for build

--- server/build/test.js
+++ server/build/test.js
@@ -1,7 +1,39 @@
+const { join } = require('path')
 const assert = require('assert')
+const fs = require('fs')
+
+const shelljs = require('shelljs')
+const config = require('config')
 
 describe('server/build', () => {
-  it('should pass test', () => {
-    assert(true)
+  const createBuild = require('./createBuild.js')
+
+  const pr = {
+    number: 102,
+    head: {
+      ref: 'master',
+      repo: {
+        clone_url: join(__dirname, '../..')
+      }
+    }
+  }
+  const workspace = join(config.get('workDir'), '100')
+
+  before(() => {
+    createBuild(pr)
+  })
+
+  it('should create workspace directory', done => {
+    fs.access(workspace, fs.constants.R_OK, err => {
+      assert.ifError(err)
+      done(err)
+    })
+  })
+
+  it('should have project cloned in workspace', done => {
+    fs.access(join(workspace, '.git'), fs.constants.R_OK, err => {
+      assert.ifError(err)
+      done(err)
+    })
   })
 })
