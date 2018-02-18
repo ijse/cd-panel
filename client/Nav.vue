c9b86fe436a2cbe3cb3fb777f5ae3a00050a2dd4
ijse <i@ijser.cn>
2018-02-19 01:11:33 +0800

    add burger to navbar

--- client/Nav.vue
+++ client/Nav.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="navbar">
+  <div class="navbar is-fixed-top">
     <div class="navbar-brand">
       <router-link to="/" class="navbar-item">
         / CDPanel /
       </router-link>
+      <div class="navbar-burger" :class="{ 'is-active': burgerOn }"
+        @click="burgerOn = !burgerOn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
-    <div class="navbar-menu">
+    <div class="navbar-menu" :class="{ 'is-active': burgerOn }">
       <div class="navbar-start">
         <router-link to="/mr" class="navbar-item"> MR </router-link>
         <router-link to="/backlog" class="navbar-item"> Backlog </router-link>
@@ -16,3 +22,11 @@
   </div>
 </template>
 
+<script>
+  export default {
+    name: 'NavBar',
+    data: () => ({
+      burgerOn: false
+    })
+  }
+</script>
