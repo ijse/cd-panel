55af73224c2dc4dc78c3b321f49056fa4874516f
ijse <i@ijser.cn>
2018-02-19 02:42:43 +0800

    update dashboard

--- client/Dashboard/index.vue
+++ client/Dashboard/index.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="dashboard">
-    this is dashboard
+    <nav class="level">
+      <div class="level-item has-text-centered"
+        v-for="stats in statsList">
+        <div>
+          <p class="heading">{{ stats.name }}</p>
+          <p class="title">{{ stats.value }}</p>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 <script>
   export default {
     name: 'Dashboard',
     sockets: {
-      news (data) {
-        console.log('>>', data)
+      mrs (list) {
+        this.statsList.prCount.value = list.length
       }
-    }
+    },
+    data: () => ({
+      statsList: {
+        prCount: { name: 'Pull Requests', value: 23 },
+        visitCount: { name: 'Visit Count', value: 21 },
+        deployCount: { name: 'Deploy Times', value: 23 },
+        QueueSize: { name: 'Tasks in Queue', value: 23 }
+      }
+    })
   }
 </script>
