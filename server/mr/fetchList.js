bd8ebc76dd8e20362dec0fda0b257bb16af2d2ea
Li Yi <i@ijser.cn>
2018-03-15 19:54:31 +0800

    fix reviewers check

--- server/mr/fetchList.js
+++ server/mr/fetchList.js
@@ -12,15 +12,20 @@ async function updateReviewStatus(pr) {
       number: pr.number
     })
 
-  const reviewers = pr.requested_reviewers.map(r => r.login)
+  // const reviewers = pr.requested_reviewers.map(r => r.login)
 
   const reviewResult = {}
-  data.forEach(d => reviewResult[d.user.login] = d.state === 'APPROVED')
+  data
+    // since last update
+    .filter(d => - (new Date(pr.updated_at)) + (new Date(d.submitted_at)))
+    .forEach(d => reviewResult[d.user.login] = d.state === 'APPROVED')
 
-  pr.isApproved = reviewers.every(r => reviewResult[r])
+  const reviewers = Object.keys(reviewResult)
+  pr.isApproved = !!reviewers.length && reviewers.every(r => reviewResult[r])
 
   if (pr.number == 3065) {
     console.log('>>>', pr.isApproved, data)
+    console.log('>>>>', reviewResult, pr)
   }
   return pr
 }
